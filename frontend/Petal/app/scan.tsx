f57c72c98e8022eb2cda7c323cb187d41d20ebd3
Sharad Agrawal <sharada.2021@scis.smu.edu.sg>
2024-08-31 15:53:33 +0800

    fix: file structure

--- frontend/Petal/app/scan.tsx
+++ frontend/Petal/app/scan.tsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { Link } from "expo-router";
-import {
-  Button,
-  Image,
-  View,
-  StyleSheet,
-  Dimensions,
-  Alert,
-  Text,
-} from "react-native";
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Button, Image, View, StyleSheet, Dimensions, Alert, Platform, Pressable, Text, ActivityIndicator, ScrollView } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import Spinner from 'react-native-loading-spinner-overlay';
+// import { IndexPath, Layout, Select, SelectItem } from '@ui-kitten/components';
+import { Dropdown } from 'react-native-element-dropdown';
+import { ThemedText } from '@/components/ThemedText';
+import {Overlay} from "react-native-elements";
 
-import * as ImagePicker from "expo-image-picker";
-import * as FileSystem from "expo-file-system";
+const data = [
+  { label: 'Recycling Information', value: 1 },
+  { label: 'Upcycle Ideas', value: 2 },
+  { label: 'Fun Facts!', value: 3 },
+];
 
 export default function Scan() {
-
   const win = Dimensions.get("window");
+  const [isLoading, setLoading] = useState(false);
   const [image, setImage] = useState<string | null>(null);
   const [ratio, setRatio] = useState(1);
+  const [selectedValue, setValue] = useState(0)
+  const [funfacts, setFunFacts] = useState();
+  const [upcycle, setUpcycle] = useState();
+  const [classification, setClassification] = useState();
+  // const [selectedIndex, setSelectedIndex] = React.useState<IndexPath | IndexPath[]>();
+  const [displayedText, setText] = useState()
+  const [visible, setVisible] = useState(false);
   const scale = 0.8;
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -30,113 +42,189 @@ export default function Scan() {
 
     if (!result.canceled) {
       const { uri, width, height } = result.assets[0];
+      // console.log(uri)
       setImage(uri);
+      // console.log(image)
       setRatio(height / width);
 
-      try {
-        // Read the file as base64
-        const fileBase64 = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
-        // const base64Data = await readFile(uri, 'base64')
-        // Create FormData
-        const formData = new FormData();
-        formData.append('file', fileBase64);
-        
-        // Make the request to FastAPI endpoint
-        const response = await fetch('https://petalbackend.onrender.com/upload', {
-          method: 'POST',
-          body: formData,
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-
-        if (!response.ok) {
-            console.log(response.status)
-          throw new Error('Network response was not ok');
-        }
-
-        const output = await response.json();
-        // console.
-        console.log('Generated content:', output.fun_facts);
-        Alert.alert('Fun Facts', output.fun_facts);
-      } catch (error) {
-        console.error('Error:', error);
-        Alert.alert('Error', 'Failed to upload image and generate content.');
-      }
+      // uploadImage()
+      // setLoading(true)
     }
   };
+
+  useEffect(() => {
+    // action on update of movies
+    if (image == null) return;
+    setLoading(true)
+    uploadImage()
+  }, [image]);
+
+  const uploadImage = async () => {
+    try {
+      // Read the file as base64
+      // console.log(image)
+      const uri = image
+      const fileBase64 = await FileSystem.readAsStringAsync(uri!, { encoding: FileSystem.EncodingType.Base64 });
+      // const base64Data = await readFile(uri, 'base64')
+      // Create FormData
+      const formData = new FormData();
+      formData.append('file', fileBase64);
+
+      // Make the request to FastAPI endpoint
+      const response = await fetch('https://petalbackend.onrender.com/upload', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      const output = await response.json();
+      setLoading(false)
+
+      if (!response.ok) {
+        // console.log(response.status)
+        // console.log(response.body)
+        throw new Error(output);
+      }
+
+      console.log('Generated content:', output.fun_facts);
+      // Alert.alert('Fun Facts', output.classification);
+      setClassification(output.classification)
+      setFunFacts(output.fun_facts)
+      setUpcycle(output.upcycle)
+
+    } catch (error) {
+      console.error('Error:', error);
+      Alert.alert('Error', 'Failed to upload image and generate content.');
+    }
+  }
+
+  function handleSelectChange(e: { label?: string; value: any; }): void {
+    setVisible(true)
+    setValue(e.value)
+    if (e.value == 1) {
+      setText(classification)
+    } else if (e.value == 2) {
+      setText(upcycle)
+    } else if (e.value == 3) [
+      setText(funfacts)
+    ]
+  }
 
   const width = scale * win.width;
   const height = ratio * width;
 
   return (
-    <View style={styles.container}>
-              <Image
-          source={require('../assets/images/topbanner.jpg')}
-          style={styles.banner}
-        />
-      {/* Body content */}
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width, height }} />}
-      {/* Bottom Navbar */}
-      <View style={styles.navbar}>
-        <Link href="/home" style={styles.navButton}>
-          <View style={styles.iconTextContainer}>
-            <Ionicons name="home-outline" size={24} color="black" />
-            <Text>Home</Text>
-          </View>
-        </Link>
-        <Link href="/Scan" style={styles.navButton}>
-          <View style={styles.iconTextContainer}>
-            <Ionicons name="search-outline" size={24} color="black" />
-            <Text>Scan</Text>
-          </View>
-        </Link>
-        <Link href="/location" style={styles.navButton}>
-          <View style={styles.iconTextContainer}>
-            <Ionicons name="location-outline" size={24} color="black" />
-            <Text>Locate</Text>
-          </View>
-        </Link>
-        <Link href="/rewards" style={styles.navButton}>
-          <View style={styles.iconTextContainer}>
-            <Ionicons name="gift-outline" size={24} color="black" />
-            <Text>Rewards</Text>
-          </View>
-        </Link>
-        <Link href="/Faq" style={styles.navButton}>
-          <View style={styles.iconTextContainer}>
-            <Ionicons name="help-outline" size={24} color="black" />
-            <Text>FAQ</Text>
-          </View>
-        </Link>
+    <View>
+      
+      <Spinner
+        visible={isLoading}
+        textContent={'Loading...'}
+        textStyle={styles.spinnerTextStyle}
+      />
+      <View style={styles.imageUP}>
+        {image != null ? <Image source={{ uri: image }} style={{ width, height }} /> : null}
       </View>
-    </View>
+      <Pressable style={styles.uploadButton} onPress={pickImage}>
+        <Text style={{ fontSize: 16, color: 'white' }}>{image ? "Pick new image from camera roll" : "Pick an image from camera roll"}</Text>
+      </Pressable>
+      {/* <Select
+        style={styles.select}
+        placeholder='Disabled'
+        disabled={true}
+      >
+        <SelectItem title='Recycling Information' />
+        <SelectItem title='Upcycle Ideas' />
+        <SelectItem title='Fun Facts!' />
+      </Select> */}
+      <Dropdown style={[styles.dropdown, { backgroundColor: (image == null || isLoading) ? "lightgray" : "white" }]}
+        disable={image == null || isLoading}
+        data={data}
+        labelField="label"
+        valueField="value"
+        placeholder={(image == null || isLoading) ? "Please upload an image" : 'What information are you looking for?'}
+        onChange={e =>
+          handleSelectChange(e)
+        }
+        dropdownPosition='top'
+      />
+      {/* <ScrollView> */}
+        {/* <ThemedText style={styles.displayText}>{displayedText}</ThemedText> */}
+      {/* </ScrollView> */}
+
+      {/* {!(image != null && classification == null) ? null : <ActivityIndicator size={50} />} */}
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={styles.overlaystyle} >
+        <Text>{displayedText}</Text>
+      </Overlay>
+    </View >
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  overlaystyle: {
+    // margin:15,
+    // marginEnd?
+    margin:50,
+    padding:10,
+    minWidth:"80%",
+    borderRadius:10
+    // backgroundColor:"black"
+  },
+  displayText: {
+    fontSize: 16,
+    // fontFamily:"lato"
+    bottom: -100
+  },
+  imageUP: {
     flex: 1,
-    justifyContent: "space-between",
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: -275
   },
-  navbar: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    backgroundColor: "#fff",
-    paddingVertical: 10,
-    borderTopWidth: 1,
-    borderTopColor: "#ccc",
+  uploadButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+    top: 760,
+    left: 60,
+    // x
+    width: 300
   },
-  navButton: {
-    alignItems: "center",
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  iconTextContainer: {
-    alignItems: "center",
+  spinnerTextStyle: {
+    color: '#FFF'
   },
-  banner: {
-    width: '100%',
-    height: 180,
-    resizeMode: 'cover',
+  dropdown: {
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    margin: 16,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+    bottom: -630,
   },
 });
